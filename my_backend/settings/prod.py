e5020d192e3880065a2c2f4f88b0b06c05f55d18
Yougotcrushed <gettycrushed@gmail.com>
2025-04-29 16:16:20 +0100

    Fix DATABASES configuration for Heroku

--- my_backend/settings/prod.py
+++ my_backend/settings/prod.py
@@ -9,4 +9,8 @@ ALLOWED_HOSTS = ['realestate-prod-fdf77c6bb00a.herokuapp.com']
 
 SECRET_KEY = os.environ['SECRET_KEY']
 
-DATABASES = dj_database_url.config()
+DATABASES = {
+    'default': dj_database_url.config()
+}
+
+
